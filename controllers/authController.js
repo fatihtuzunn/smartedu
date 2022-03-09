4af907450f8306d15c39b515c4bb08c2b0a222d9
fatihtuzunn <fatihtuzun11@gmail.com>
2022-03-09 19:25:21 +0300

    new auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,10 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
-const session = require('express-session');
+const express = require("express");
+const router = express.Router();
 
 
-
+/* 
 exports.createUser = async (req, res) => {
     
     try {
@@ -39,6 +40,8 @@ exports.loginUser =  async (req, res) => {
             }
         })
 
+
+        
         
     } catch (error) {
         res.status(400).json({
@@ -46,6 +49,39 @@ exports.loginUser =  async (req, res) => {
             error,
         });      
     }
-}
+} */
 
 
+  // signup route
+  exports.createUser= async (req, res) => {
+    const body = req.body;
+
+    if (!(body.email && body.password)) {
+      return res.status(400).send({ error: "Data not formatted properly" });
+    }
+
+    // creating a new mongoose doc from user data
+    const user = new User(body);
+    // generate salt to hash password
+    const salt = await bcrypt.genSalt(10);
+    // now we set user password to hashed password
+    user.password = await bcrypt.hash(user.password, salt);
+    user.save().then((doc) => res.status(201).send(doc));
+  }
+
+  // login route
+  exports.loginUser = async (req, res) => {
+    const body = req.body;
+    const user = await User.findOne({ email: body.email });
+    if (user) {
+      // check user password with hashed password stored in the database
+      const validPassword = await bcrypt.compare(body.password, user.password);
+      if (validPassword) {
+        res.status(200).json({ message: "Valid password" });
+      } else {
+        res.status(400).json({ error: "Invalid Password" });
+      }
+    } else {
+      res.status(401).json({ error: "User does not exist" });
+    }
+  }
